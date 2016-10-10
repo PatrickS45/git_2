a88863f2aa6458a65b3c91048c9541134ae530e9
PatrickS45 <tuko45@hotmail.fr>
2016-10-10 17:21:56 +0200

    Maj pour quete CRUD

---
+++ src/WCS/CoavBundle/Entity/Reservation.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace WCS\CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Reservation
+ */
+class Reservation
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $nbSeats;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Reservation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nbSeats
+     *
+     * @param string $nbSeats
+     * @return Reservation
+     */
+    public function setNbSeats($nbSeats)
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbSeats
+     *
+     * @return string 
+     */
+    public function getNbSeats()
+    {
+        return $this->nbSeats;
+    }
+}
